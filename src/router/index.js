2094dec23ff93ede7042056ce27e687c90c8881f
BucBharat <58068566+BucBharat@users.noreply.github.com>
2022-04-15 17:43:02 +0530

    stage2

--- src/router/index.js
+++ src/router/index.js
@@ -5,36 +5,32 @@ import Login from '../views/Login.vue';
 import Users from '../views/Users.vue';
 
 Vue.use(VueRouter);
-
+const authentication = (to, from, next) => {
+  // JWT verification goes here
+  if (localStorage.getItem('accessToken')) {
+    next();
+  } else {
+    next({
+      name: 'Login',
+    });
+  }
+};
 const routes = [
+  {
+    path: '/',
+    name: 'Login',
+    component: Login,
+  },
   {
     path: '/login',
     name: 'Login',
     component: Login,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
-  {
     path: '/users',
     name: 'Users',
     component: Users,
-    beforeEnter(to, from, next) {
-      // JWT verification goes here
-      if (localStorage.getItem('accessToken')) {
-        next();
-      } else {
-        next({
-          name: 'Login',
-        });
-      }
-    },
+    beforeEnter: authentication,
   },
 ];
 
